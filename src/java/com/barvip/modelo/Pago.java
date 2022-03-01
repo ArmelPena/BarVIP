08d5617ed6733a30c65896c5bf193d2337a68deb
ArmelPena <armelpena@hotmail.com>
2022-03-01 18:38:20 -0500

    Actualizacion de Pagos

--- src/java/com/barvip/modelo/Pago.java
+++ src/java/com/barvip/modelo/Pago.java
@@ -12,13 +12,21 @@ package com.barvip.modelo;
 public class Pago {
     private int IdPago;
     private Double Monto;
+    private String NombreCliente;
+    private String Direccion;
+    private String Telefono;
+    private String ReferenciaDir;
 
     public Pago() {
     }
 
-    public Pago(int IdPago, Double Monto) {
+    public Pago(int IdPago, Double Monto, String NombreCliente, String Direccion, String Telefono, String ReferenciaDir) {
         this.IdPago = IdPago;
         this.Monto = Monto;
+        this.NombreCliente = NombreCliente;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+        this.ReferenciaDir = ReferenciaDir;
     }
 
     public int getIdPago() {
@@ -36,5 +44,41 @@ public class Pago {
     public void setMonto(Double Monto) {
         this.Monto = Monto;
     }
+
+    public String getNombreCliente() {
+        return NombreCliente;
+    }
+
+    public void setNombreCliente(String NombreCliente) {
+        this.NombreCliente = NombreCliente;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getReferenciaDir() {
+        return ReferenciaDir;
+    }
+
+    public void setReferenciaDir(String ReferenciaDir) {
+        this.ReferenciaDir = ReferenciaDir;
+    }
+
+
+
+
     
 }
